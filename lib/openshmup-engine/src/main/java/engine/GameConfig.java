c521aeac06ad8982959dc06d3506fc4f256650aa
lmmas <129756891+lmmas@users.noreply.github.com>
2025-03-21 19:58:54 +0100

    moved the game config into its own class

---
+++ lib/openshmup-engine/src/main/java/engine/GameConfig.java
@@ -0,0 +1,30 @@
+package engine;
+
+final public class GameConfig {
+    private static int editionWidth;
+    private static int editionHeight;
+
+    public static int getEditionWidth() {
+        return editionWidth;
+    }
+
+    public static int getEditionHeight() {
+        return editionHeight;
+    }
+
+    public static void setEditionResolution(int editionWidth, int editionHeight) {
+        GameConfig.editionWidth = editionWidth;
+        GameConfig.editionHeight = editionHeight;
+    }
+
+    public static class LevelUI {
+        public static int upperLayer = 1024;
+
+        public static class Lives {
+            public static String textureFilepath;
+            public static Vec2D size;
+            public static Vec2D position;
+            public static Vec2D stride;
+        }
+    }
+}
