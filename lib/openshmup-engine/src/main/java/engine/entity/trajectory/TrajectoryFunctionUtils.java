2264c75555b72a4b3f56ad565b05de8f765c4ac9
lmmas <129756891+lmmas@users.noreply.github.com>
2025-03-22 21:51:25 +0100

    Partial entity refactor, using Suppliers instead of Functions for storage

---
+++ lib/openshmup-engine/src/main/java/engine/entity/trajectory/TrajectoryFunctionUtils.java
@@ -0,0 +1,82 @@
+package engine.entity.trajectory;
+//DO NOT OPTIMIZE IMPORTS
+import java.util.function.Function;
+
+import static java.lang.Math.*;
+
+public class TrajectoryFunctionUtils {
+    public static float compositeIntervalFunction(float t, float[] bounds, Function<Float, Float>[] functions){
+        if(bounds.length + 1 != functions.length){
+            throw new IllegalArgumentException("function count different from interval count");
+        }
+        for(int i = 0; i < functions.length - 1; i++){
+            if(t < bounds[i]){
+                return functions[i].apply(t);
+            }
+        }
+        return functions[functions.length - 1].apply(t);
+    }
+
+    public static class MathFloatOverloads {
+    //WARNING: DO NOT DELETE
+        public static float sin(float x){
+            return (float) Math.sin(x);
+        }
+
+        public static float cos(float x){
+            return (float) Math.cos(x);
+        }
+
+        public static float tan(float x){
+            return (float) Math.tan(x);
+        }
+
+        public static float asin(float x){
+            return (float) Math.asin(x);
+        }
+
+        public static float acos(float x){
+            return (float) Math.acos(x);
+        }
+
+        public static float atan(float x){
+            return (float) Math.atan(x);
+        }
+
+        public static float log(float x){
+            return (float) Math.log10(x);
+        }
+
+        public static float sqrt(float x){
+            return (float) Math.sqrt(x);
+        }
+
+        public static float cbrt(float x){
+            return (float) Math.cbrt(x);
+        }
+
+        public static float ceil(float x){
+            return (float) Math.ceil(x);
+        }
+
+        public static float floor(float x){
+            return (float) Math.floor(x);
+        }
+
+        public static float rint(float x){
+            return (float) Math.rint(x);
+        }
+
+        public static float atan2(float y, float x){
+            return (float) Math.atan2(y, x);
+        }
+
+        public static float pow(float x, float y){
+            return (float) Math.pow(x,y);
+        }
+
+        public static float E = (float) Math.E;
+        public static float PI = (float) Math.PI;
+        public static float TAU = (float) Math.TAU;
+    }
+}
